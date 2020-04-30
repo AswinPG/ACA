8fc35c557cff5c4503e78888e1cff9260f65c625
Anjana R <44143225+AnjanaRajanM@users.noreply.github.com>
2020-04-30 15:12:25 +0530

    google authentication

---
+++ ACA/Anjana/Authentication/Model/GoogleUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACA.Anjana.Authentication.Model
+{
+    public class GoogleUser
+    {
+        public string token;
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Uri Picture { get; set; }
+        public string Token { get; set; }
+    }
+}
