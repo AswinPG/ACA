532c3f70c880f35a61ed014bc84ea9919969cf55
Anjana R <44143225+AnjanaRajanM@users.noreply.github.com>
2020-05-04 14:37:08 +0530

    animation

--- ACA/Anjana/Authentication/views/Loginpage.xaml.cs
+++ ACA/Anjana/Authentication/views/Loginpage.xaml.cs
@@ -22,52 +22,91 @@ namespace ACA.Anjana.Authentication.views
         public Loginpage()
         {
             InitializeComponent();
+           
         }
         
         public async void Login(object sender, EventArgs e)
         {
-            try
-            {
-                
-                _googleManager.Logout();
-                _googleManager.Login(OnLoginComplete);
+            Firstlight.TranslationY = -20;
+            Secondlight.TranslationY = -20;
+            Clock.TranslationY = -20;
+            View.TranslationY = -20;
+            LoginButton.TranslationY = -20;
+            Forget.TranslationY = -20;
+            Firstlight.Opacity = 0;
+            Secondlight.Opacity = 0;
+            Clock.Opacity = 0;
+            View.Opacity = 0;
+            LoginButton.Opacity = 0;
+            Forget.Opacity = 0;
+            Firstlight.FadeTo(1, 100);
+            
+            await Firstlight.TranslateTo(0, 0, 300);
+            Secondlight.FadeTo(1, 100);
+            
+            await Secondlight.TranslateTo(0, 0, 300);
+            Clock.FadeTo(1, 100);
+            
+            await Clock.TranslateTo(0, 0, 300);
+            View.FadeTo(1, 100);
+            
+            await View.TranslateTo(0, 0, 300);
+            LoginButton.FadeTo(1, 100);
+            
+            await LoginButton.TranslateTo(0, 0, 300);
+            Forget.FadeTo(1, 100);
+            
+            await Forget.TranslateTo(0, 0, 300);
 
-            }
-            catch (Exception x)
-            {
-                
-                await DisplayAlert("Authentication Failed", "Your Authentication Attempt Failed. Please try again..", "Ok");
-            }
+
+
+
+
+
+
+
+            //try
+            //{
+
+            //    _googleManager.Logout();
+            //    _googleManager.Login(OnLoginComplete);
+
+            //}
+            //catch (Exception x)
+            //{
+
+            //    await DisplayAlert("Authentication Failed", "Your Authentication Attempt Failed. Please try again..", "Ok");
+            //}
         }
 
        
 
-        private async void OnLoginComplete(GoogleUser googleUser, string message)
-        {
-            if (googleUser != null)
-            {
-                GoogleUser = googleUser;
-                try
-                {
-                    AppUser User = await DependencyService.Get<IFireBaseAuthenticator>().LoginWithGoogle(googleUser.Token, null);
-                    //Application.Current.Properties["User"] = User.Uid;
-                }
-                catch (Exception e)
-                {
-                    await DisplayAlert("Oops", "Firebase Error", "Ok");
+        //private async void OnLoginComplete(GoogleUser googleUser, string message)
+        //{
+        //    if (googleUser != null)
+        //    {
+        //        GoogleUser = googleUser;
+        //        try
+        //        {
+        //            AppUser User = await DependencyService.Get<IFireBaseAuthenticator>().LoginWithGoogle(googleUser.Token, null);
+        //            //Application.Current.Properties["User"] = User.Uid;
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            await DisplayAlert("Oops", "Firebase Error", "Ok");
                     
-                }
+        //        }
 
-                IsLogedIn = true;
-                await DisplayAlert("Success", message, "Ok");
+        //        IsLogedIn = true;
+        //        await DisplayAlert("Success", message, "Ok");
                 
-            }
-            else
-            {
+        //    }
+        //    else
+        //    {
                 
-                await DisplayAlert("Error", message, "Ok");
-            }
-        }
+        //        await DisplayAlert("Error", message, "Ok");
+        //    }
+        //}
 
         
     }
